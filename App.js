0dbab51dfa051fef9e40fdbf1381969ede20efa1
Mamad <naufaloke679@gmail.com>
2023-11-19 16:32:39 +0700

    dada

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { AboutScreen, TaskCompletedScreen, TaskScreen } from "./screens";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-// tes
+// tesdad
 const BottomNavigator = () => {
   return (
     <Tab.Navigator
